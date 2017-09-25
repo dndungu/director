c9d237ac0086ad00e7256f713c0d6b24b158ccf4
David Ndungu <dnjuguna@gmail.com>
2017-09-24 19:24:44 -0700

    Validates the POC.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM golang:1.9 as builder
+
+LABEL maintainer "David Ndungu <dnjuguna@gmail.com>"
+
+WORKDIR /bin
+
+COPY main.go .
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o facade .
+
+FROM scratch
+
+LABEL maintainer "David Ndungu <dnjuguna@gmail.com>"
+
+WORKDIR /bin
+
+COPY --from=builder /bin/facade .
+
+ENV HTTP_PROXY_PORT 80
+
+EXPOSE 80
+
+ENV HTTPS_PROXY_PORT 443
+
+EXPOSE 443
+
+ENTRYPOINT ["/bin/facade"]
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"golang.org/x/crypto/acme/autocert"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"os/signal"
+)
+
+type source struct {
+	hostname string
+	scheme   string
+}
+
+type target struct {
+	address  string
+	hostname string
+	port     int
+	scheme   string
+}
+
+type rule struct {
+	source source
+	target target
+}
+
+const (
+	HTTP  = "http"
+	HTTPS = "https"
+)
+
+var domains []string
+
+var manager autocert.Manager
+
+var defaultTarget = target{"zatiti.com", "zatiti.com", 80, HTTP}
+
+func findTarget(u *url.URL) (t *target, err error) {
+	// TODO fetch this from the data store
+	t = &defaultTarget
+	return t, err
+}
+
+func director(r *http.Request) {
+	t, err := findTarget(r.URL)
+	if err != nil {
+		t = &defaultTarget
+	}
+	r.URL.Scheme = t.scheme
+	if t.port == 80 || t.port == 443 {
+		r.URL.Host = t.address
+	} else {
+		r.URL.Host = fmt.Sprintf("%s:%s", t.address, t.port)
+	}
+	r.Header.Set("Host", t.hostname)
+	if _, ok := r.Header["User-Agent"]; !ok {
+		r.Header.Set("User-Agent", "")
+	}
+}
+
+func hostPolicy(context.Context, string) error {
+	// TODO only allow while listed domains and subdomains
+	return nil
+}
+
+func init() {
+	domains = []string{"zatiti.com"}
+	manager = autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: hostPolicy,
+	}
+}
+
+func main() {
+	proxy := httputil.ReverseProxy{Director: director}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	httpServer := http.Server{Addr: ":80", Handler: &proxy}
+	go func() {
+		log.Print(httpServer.ListenAndServe().Error())
+	}()
+
+	httpsServer := &http.Server{
+		Addr:      ":443",
+		TLSConfig: &tls.Config{GetCertificate: manager.GetCertificate},
+		Handler:   &proxy,
+	}
+
+	go func() {
+		log.Print(httpsServer.ListenAndServeTLS("", ""))
+	}()
+	<-stop
+	httpServer.Shutdown(context.Background())
+	httpsServer.Shutdown(context.Background())
+}
