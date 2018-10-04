509bc8fe804cb82a3e635a2b0f3a0e80f9401ef2
David Ndungu <dndungu@zendesk.com>
2018-10-04 14:30:14 -0700

    sets the port value as int

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func director(r *http.Request) {
 	if t.port == 80 || t.port == 443 {
 		r.URL.Host = t.address
 	} else {
-		r.URL.Host = fmt.Sprintf("%s:%s", t.address, t.port)
+		r.URL.Host = fmt.Sprintf("%s:%d", t.address, t.port)
 	}
 	if _, ok := r.Header["User-Agent"]; !ok {
 		r.Header.Set("User-Agent", "")
